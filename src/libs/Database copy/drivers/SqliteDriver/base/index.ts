08f44885fb088cc622b26f942a7a07f6fc77f5b8
SinGlEBW <bw.klan@mail.ru>
2026-09-23 17:11:28 +0300

    Release:v1.1.24

--- src/libs/Database copy/drivers/SqliteDriver/base/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './checkTableSqlite';
-export * from './dropTableSqlite';
-export * from './openDbSqlite';
-export * from './querySqlite';
-export * from './types';
-
--- src/libs/Database copy/drivers/SqliteDriver/base/types.ts
+++
@@ -1,4 +0,0 @@
-import type { BaseReturnProps } from '../../types';
-import { Database } from '../types';
-
-export type BaseFunctionSqlite = (connect: Database, nameTable: string) => Promise<BaseReturnProps & { tables?: string[] }>;
